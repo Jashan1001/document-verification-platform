e3319bba36f4ba9b016a287560757473699e3457
Jashanpreet Kaur <23BCS14043@cuchd.in>
2026-02-27 19:50:31 +0530

    Integrate Prisma client and connect database to Express app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,35 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-
+import prisma from "./config/prisma";
+import authRoutes from "./routes/auth.routes";
+import { authenticate } from "./middleware/auth.middleware";
+import { authorize } from "./middleware/rbac.middleware";
+import institutionRoutes from "./routes/institution.routes";
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/institutions", institutionRoutes);
+app.get(
+  "/admin-only",
+  authenticate,
+  authorize(["SUPER_ADMIN"]),
+  (req, res) => {
+    res.json({
+      message: "Welcome Super Admin",
+    });
+  }
+);
+app.get("/health", authenticate, async (req, res) => {
+  const usersCount = await prisma.user.count();
 
-app.get("/health", (_req, res) => {
-  res.status(200).json({ message: "Server is healthy" });
+  res.status(200).json({
+    message: "Protected health check",
+    usersCount,
+  });
 });
 
 export default app;
